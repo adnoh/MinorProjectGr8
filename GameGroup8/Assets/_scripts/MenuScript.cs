5f6da7899313c76f3805ca76256a414baa067e69
wroel <wytze.roel@gmail.com>
2015-11-12 16:56:13 +0100

    Add first basic main menu

---
+++ GameGroup8/Assets/_scripts/MenuScript.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class MenuScript : MonoBehaviour {
+	
+	public Button StartGame;
+	public Button ExitGame;
+
+	void start() {
+		StartGame = StartGame.GetComponent<Button> ();
+		ExitGame = ExitGame.GetComponent<Button> ();
+	}
+
+	public void StartPress() {
+		Application.LoadLevel ("Game");
+	}
+
+	public void ExitPress() {
+		Application.Quit ();
+	}
+
+}
