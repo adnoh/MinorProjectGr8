0aacf822894b33f8dfe1189a05b24d0402d94936
wroel <wytze.roel@gmail.com>
2015-11-13 16:30:03 +0100

    Add slider and name field to main menu

--- GameGroup8/Assets/Scripts/MenuScript.cs
+++ GameGroup8/Assets/Scripts/MenuScript.cs
@@ -1,17 +1,40 @@
 ﻿using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.Audio;
 using System.Collections;
 
 public class MenuScript : MonoBehaviour {
-	
-	public Button StartGame;
-	public Button ExitGame;
-	public Button OpenOptions;
 
+	public Button startGame;
+	public Button exitGame;
+
+	public Slider slider;
+
+	public InputField name;
 
 	void start() {
-		StartGame = StartGame.GetComponent<Button> ();
-		ExitGame = ExitGame.GetComponent<Button> ();
+
+		startGame = startGame.GetComponent<Button> ();
+		exitGame = exitGame.GetComponent<Button> ();
+
+		slider = slider.GetComponent<Slider> ();
+
+		if (!PlayerPrefs.HasKey ("Volume")) {
+			PlayerPrefs.SetFloat ("Volume", 1);
+		}
+
+		slider.value = PlayerPrefs.GetFloat ("Volume");
+
+		AudioListener.volume = PlayerPrefs.GetFloat ("Volume");
+
+		name = name.GetComponent<InputField> ();
+
+		if (!PlayerPrefs.HasKey ("PlayerName")) {
+			PlayerPrefs.SetString ("PlayerName", "");
+		}
+
+		name.text = PlayerPrefs.GetString ("PlayerName");
+
 	}
 
 	public void StartPress() {
@@ -21,5 +44,15 @@ public class MenuScript : MonoBehaviour {
 	public void ExitPress() {
 		Application.Quit ();
 	}
+
+	public void setSound() {
+		AudioListener.volume = slider.normalizedValue;
+		PlayerPrefs.SetFloat ("Volume", slider.normalizedValue);
+	}
+
+	public void changeName() {
+		PlayerPrefs.SetString ("PlayerName", name.text);
+	}
+
 
 }
