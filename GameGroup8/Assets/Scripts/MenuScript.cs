4708033381734e270f128943a43877e7e2c200fa
wroel <wytze.roel@gmail.com>
2015-11-13 14:45:13 +0100

    Add changes to main menu

--- GameGroup8/Assets/Scripts/MenuScript.cs
+++ GameGroup8/Assets/Scripts/MenuScript.cs
@@ -3,13 +3,18 @@ using UnityEngine.UI;
 using System.Collections;
 
 public class MenuScript : MonoBehaviour {
-	
+
+	public Canvas OptionsScreen;
 	public Button StartGame;
 	public Button ExitGame;
+	public Button OpenOptions;
+
 
 	void start() {
 		StartGame = StartGame.GetComponent<Button> ();
 		ExitGame = ExitGame.GetComponent<Button> ();
+		OptionsScreen = OptionsScreen.GetComponent<Canvas> ();
+		OptionsScreen.enabled = false;
 	}
 
 	public void StartPress() {
@@ -19,5 +24,12 @@ public class MenuScript : MonoBehaviour {
 	public void ExitPress() {
 		Application.Quit ();
 	}
+
+	public void OptionsPress() {
+		OptionsScreen.enabled = true;
+		StartGame.enabled = false;
+		ExitGame.enabled = false;
+	}
+
 
 }
