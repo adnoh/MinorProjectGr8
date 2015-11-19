f522fe2539ed439135fd15fcfcb0f6a2e9a7f556
wroel <wytze.roel@gmail.com>
2015-11-19 14:39:12 +0100

    Dont know

--- GameGroup8/Assets/Scripts/EnemySpawner.cs
+++ GameGroup8/Assets/Scripts/EnemySpawner.cs
@@ -3,7 +3,12 @@ using System.Collections;
 
 public class EnemySpawner : MonoBehaviour {
 
+	public GameObject normalEnemy;
 
-	
+	void Update(){
+		if(Input.GetKeyDown (KeyCode.P)){
+			//Instantiate (normalEnemy, new Vector3(-13, 1, -1), Quaternion.identity);
+		}
+	}
 
 }
