f075fae0b73adb7d2d7287bd5bcadd5a2f774d8d
wroel <wytze.roel@gmail.com>
2015-11-20 15:51:06 +0100

    Fix shooting mechanism

--- GameGroup8/Assets/Scripts/PlayerAttacker.cs
+++ GameGroup8/Assets/Scripts/PlayerAttacker.cs
@@ -11,65 +11,36 @@ public class PlayerAttacker : MonoBehaviour {
 	public Text enemyHealthBar;
 	public Text enemyWeaponDamageText;
 
-	private GameObject enemy;
-
-	private EnemyController enemyController;
-
 	private int attackStrength;
 	
 	void Start () {
-		enemyController = null;
 		showEnemyDescription = false;
 		enemyDescription.SetActive (false);
-		enemy = null;
 		attackStrength = 25;
 	}
 
 	void Update () {
 		enemyDescription.SetActive (showEnemyDescription);
-		if (enemyController != null) {
-			enemyDescriptionText.text = "Enemy Level: " + enemyController.getLevel ();
-			enemyHealthBar.text = "Health: " + enemyController.getHealth ();
-			enemyWeaponDamageText.text = "Weapon Damage: " + enemyController.getAttackPower();
-		}
 		if(Input.GetKeyDown (KeyCode.T)){
              
 			RaycastHit hit;
 			Ray bulletray = new Ray(transform.position, Vector3.forward);
 			if(Physics.Raycast(bulletray, out hit)){
 				if(hit.collider.tag == "Enemy"){
-			enemyController.setHealth(enemyController.getHealth () - attackStrength);
-			enemyHealthBar.text = "Health = " + enemyController.getHealth ();
-			if(enemyController.getHealth () <= 0){
-				EnemySpawner.enemiesDefeaten++;
-				Destroy(enemy);
-				showEnemyDescription = false;
-			}
+					Debug.Log ("Enemy");
+					EnemyController enemyController = hit.collider.gameObject.GetComponent<EnemyController>();
+					enemyController.setHealth(enemyController.getHealth () - attackStrength);
+					enemyDescriptionText.text = "Level = " + enemyController.getLevel ();
+					enemyHealthBar.text = "Health = " + enemyController.getHealth ();
+					enemyWeaponDamageText.text = "Weapon Damage = " + enemyController.getAttackPower ();
+					showEnemyDescription = true;
+					if(enemyController.getHealth () <= 0){
+						EnemySpawner.enemiesDefeaten++;
+						Destroy(hit.collider.gameObject);
+						showEnemyDescription = false;
+					}
 				}
 			}
 		}
 	}
-
-
-	void OnTriggerEnter(Collider col) {
-		if (col.gameObject.CompareTag ("Enemy")) {
-			enemyController = col.GetComponent<EnemyController> ();
-			enemy = col.gameObject;
-			showEnemyDescription = true;
-		}
-	}
-
-	void OnTriggerExit(Collider col) {
-		if (col.gameObject.CompareTag ("Enemy")) {
-			showEnemyDescription = false;
-			enemy = null;
-		}
-	}
-
-
-
-
-
-
-	
 }
