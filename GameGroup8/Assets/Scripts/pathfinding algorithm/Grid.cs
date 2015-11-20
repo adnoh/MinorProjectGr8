59bbe3ca5c4882f4f075545d7bc6c1767a312b3f
Mr-Pingu <karstenvanherwaarden@gmail.com>
2015-11-20 15:54:54 +0100

    Pathfinding algorithm fixed
    Finally working :)

--- GameGroup8/Assets/Scripts/pathfinding algorithm/Grid.cs
+++ GameGroup8/Assets/Scripts/pathfinding algorithm/Grid.cs
@@ -2,29 +2,22 @@
 using System.Collections;
 using System.Collections.Generic;
 
-/* Class that makes  wall detection possible
-
-    assumes the worldplane is in x, z plane.
-*/
-
-public class Grid : MonoBehaviour {
-
+public class Grid : MonoBehaviour
+{
 
     public Node SeekerNode;
     public Node TargetNode;
     public Node CurrentNode;
-    //public Transform Target;
-    //public Transform Seeker;
+
+
     public LayerMask WallMask;
     public Vector3 gridWorldSize;
     public float nodeRadius;
-    Node[,] grid;   
+    Node[,] grid;
 
     float nodeDiameter;
     int gridSizeX;
     int gridSizeZ;
-
-    public List<Node> path;
 
     void Start()
     {
@@ -41,110 +34,106 @@ public class Grid : MonoBehaviour {
         grid = new Node[gridSizeX, gridSizeZ];
         // get the position of bottomleft cornet: get center minus left edge  - upper edge
         Vector3 worldBottomLeft = transform.position - Vector3.right * gridWorldSize.x / 2 - Vector3.forward * gridWorldSize.z / 2;
-        
+
         for (int x = 0; x < gridSizeX; x++)
         {
             for (int z = 0; z < gridSizeZ; z++)
             {
-                Vector3 worldPosition = worldBottomLeft + Vector3.right * (x * nodeDiameter + nodeRadius) + Vector3.forward * (z * nodeDiameter + nodeRadius);
-                bool walkable = !(Physics.CheckSphere(worldPosition, nodeRadius, WallMask ));
-                grid[x,z] = new Node(walkable, worldPosition, x,z);   
-           }
+                Vector3 worldPoint = worldBottomLeft + Vector3.right * (x * nodeDiameter + nodeRadius) + Vector3.forward * (z * nodeDiameter + nodeRadius);
+                bool walkable = !(Physics.CheckSphere(worldPoint, nodeRadius, WallMask));
+                grid[x, z] = new Node(walkable, worldPoint, x, z);
+            }
         }
     }
 
-    public List<Node> GetNeighbours(Node nd) { 
+    public List<Node> GetNeighbours(Node node)
+    {
         List<Node> neighbours = new List<Node>();
 
+
         // scan neighbours loop
-        for (int x = -1; x <=1; x++)
+        for (int x = -1; x <= 1; x++)
         {
-            for (int z = -1; z <= 1; z++ )
+            for (int z = -1; z <= 1; z++)
             {
+
                 // is current node -> skip
                 if (x == 0 && z == 0)
                 {
                     continue;
                 }
 
-                int checkX = nd.gridX + x;
-                int checkZ = nd.gridX + z;
+
+                int checkX = node.gridX + x;
+                int checkZ = node.gridZ + z;
 
                 // check against end of map
 
                 if (checkX >= 0 && checkX < gridSizeX && checkZ >= 0 && checkZ < gridSizeZ)
                 {
                     neighbours.Add(grid[checkX, checkZ]);
-                    //print("neighbours added");
                 }
             }
         }
+
         return neighbours;
     }
-
     // Method that changes a real/world position into a node position
 
-    public Node WorldPositionToNode(Vector3 worldPosition)
+    public Node NodeFromWorldPoint(Vector3 worldPosition)
     {
-        float PercentX = Mathf.Clamp01((worldPosition.x + gridWorldSize.x / 2) / gridWorldSize.x);
-        float PercentZ = Mathf.Clamp01((worldPosition.z + gridWorldSize.z / 2) / gridWorldSize.z);
+        float percentX = (worldPosition.x + gridWorldSize.x / 2) / gridWorldSize.x;
+        float percentZ = (worldPosition.z + gridWorldSize.z / 2) / gridWorldSize.z;
+        percentX = Mathf.Clamp01(percentX);
+        percentZ = Mathf.Clamp01(percentZ);
 
-        int x = Mathf.RoundToInt((gridSizeX - 1) * PercentX);
-        int z = Mathf.RoundToInt((gridSizeZ - 1) * PercentZ);
-        return grid[x,z];
+        int x = Mathf.RoundToInt((gridSizeX - 1) * percentX);
+        int z = Mathf.RoundToInt((gridSizeZ - 1) * percentZ);
+        return grid[x, z];
     }
 
-    
-
-
     // visual check for grid
+    public List<Node> path;
     void OnDrawGizmos()
-    {        
-        Gizmos.DrawWireCube(transform.position, new Vector3(gridWorldSize.x, 1 , gridWorldSize.z));
-        
-        if (grid != null) {
+    {
+        Gizmos.DrawWireCube(transform.position, new Vector3(gridWorldSize.x, 1, gridWorldSize.z));
 
-            // 2 nodes that define target and seeker
+        if (grid != null)
+        {
             foreach (Node n in grid)
             {
-                Gizmos.color = (n.Walkable) ? Color.white : Color.red;
 
-                // Seeker node turns blue
-                if (SeekerNode == n)
-                {
-                    Gizmos.color = Color.blue;
-                }
-                
-                
-                                
-                // set target node yellow
-                if (TargetNode == n)
-                {
-                    Gizmos.color = Color.yellow;
-                }
+                Gizmos.color = (n.walkable) ? Color.white : Color.red;
 
                 if (CurrentNode == n)
                 {
                     Gizmos.color = Color.magenta;
                 }
 
-                
-                if (path != null)
-                {
 
+                if (path != null)
                     if (path.Contains(n))
                     {
+
+
                         Gizmos.color = Color.green;
                     }
-                }
-                
-                // Drawcubes as visual aid          
-                Gizmos.DrawCube(n.WorldPosition, Vector3.one * (nodeDiameter - .1f));
 
-                
+                // Seeker node turns blue
+                if (SeekerNode == n)
+                {
+                    Gizmos.color = Color.blue;
+                }
+
+
+                // set target node yellow
+                if (TargetNode == n)
+                {
+                    Gizmos.color = Color.yellow;
+                }
+                // Drawcubes as visual aid 
+                Gizmos.DrawCube(n.worldPosition, Vector3.one * (nodeDiameter - .1f));
             }
         }
-
     }
-
 }
--- GameGroup8/Assets/Scripts/pathfinding algorithm/Pathfinding.cs
+++ GameGroup8/Assets/Scripts/pathfinding algorithm/Pathfinding.cs
@@ -2,130 +2,110 @@
 using System.Collections;
 using System.Collections.Generic;
 
-public class Pathfinding : MonoBehaviour {
+public class Pathfinding : MonoBehaviour
+{
 
     public Transform seeker;
     public Transform target;
-    Grid grid;
 
-	// Script must be on the same gameobject to work 
-	void Awake(){
+    Grid grid;
+    // Script must be on the same gameobject to work 
+    void Awake()
+    {
         grid = GetComponent<Grid>();
-	}
+    }
 
     void Update()
     {
         FindPath(seeker.position, target.position);
     }
-	
-    void FindPath(Vector3 StartPosition, Vector3 TargetPosition)
-    {
 
-        Node startNode = grid.WorldPositionToNode(StartPosition);       
-        Node targetNode = grid.WorldPositionToNode(TargetPosition);
+    void FindPath(Vector3 startPos, Vector3 targetPos)
+    {
         
-        // Set TargetNode
-        // Set SeekerNode
+        Node startNode = grid.NodeFromWorldPoint(startPos);
+        Node targetNode = grid.NodeFromWorldPoint(targetPos);
+
+        // Set TargetNode in grid for visual feedback
+        // Set SeekerNode in grid for visual feedback
         grid.TargetNode = targetNode;
         grid.SeekerNode = startNode;
-        // print(targetNode.toString());
-        // print(startNode.toString());
 
-        List<Node> openSet = new List<Node>();
-        HashSet<Node> closedSet = new HashSet<Node>();
+        List<Node> openSet = new List<Node>();        
+        List<Node> closedSet = new List<Node>();
+
         openSet.Add(startNode);
-
 
         while (openSet.Count > 0)
         {
             Node currentNode = openSet[0];
-            grid.CurrentNode = currentNode;
 
-            // loop through the openSet to find lowest fCost
-            // if fcost is equal, look at hCost(distance to target)
-            // 
-
+            // zoek de goedkoopste van de openset -> Dit wordt nieuwe currentNode
             for (int i = 1; i < openSet.Count; i++)
             {
                 if (openSet[i].fCost < currentNode.fCost || openSet[i].fCost == currentNode.fCost && openSet[i].hCost < currentNode.hCost)
                 {
                     currentNode = openSet[i];
-                    // grid.CurrentNode = currentNode;
-                    // print(currentNode.toString());
-                    
-                } 
+                }
             }
 
             openSet.Remove(currentNode);
             closedSet.Add(currentNode);
 
-            // if currentnode reached the target, return this node
             if (currentNode == targetNode)
             {
-
-                print("Target found");
                 BacktracePath(startNode, targetNode);
-                return;
+                break;
             }
 
+            // voeg de buren toe aan de openset inclusief kosten
             foreach (Node neighbour in grid.GetNeighbours(currentNode))
             {
-
                 // if node is not walkable or in the closed set -> skip
-                if (!neighbour.Walkable || closedSet.Contains(neighbour))
+                if (!neighbour.walkable || closedSet.Contains(neighbour))
                 {
                     continue;
                 }
 
-                int newMovementCostToNeighbor = currentNode.gCost + GetDistance(currentNode, neighbour);
-                if (newMovementCostToNeighbor < neighbour.gCost || !openSet.Contains(neighbour))
+                // bereken de kosten van de nieuwe buren
+                int newMovementCostToNeighbour = currentNode.gCost + GetDistance(currentNode, neighbour);
+                // voeg alleen toe als kosten lager zijn of anders nog niet in openSet zit
+                if (newMovementCostToNeighbour < neighbour.gCost || !openSet.Contains(neighbour))
                 {
-                    neighbour.gCost = newMovementCostToNeighbor;
-                    neighbour.hCost = GetDistance(currentNode, targetNode);
+                    neighbour.gCost = newMovementCostToNeighbour;
+                    neighbour.hCost = GetDistance(neighbour, targetNode);
                     neighbour.parent = currentNode;
 
                     if (!openSet.Contains(neighbour))
-                    {
                         openSet.Add(neighbour);
-                    }
-                 
                 }
             }
-
         }
     }
 
-    void BacktracePath (Node startNode, Node endNode)
+    void BacktracePath(Node startNode, Node endNode)
     {
         List<Node> path = new List<Node>();
         Node currentNode = endNode;
-        
 
         while (currentNode != startNode)
         {
             path.Add(currentNode);
             currentNode = currentNode.parent;
-
         }
         path.Reverse();
 
         grid.path = path;
+
     }
 
     int GetDistance(Node nodeA, Node nodeB)
     {
-        // diagonal move cost 14
-        // normal move cost 10
-        // can be expanded for normal cost on certain terrain types
+        int dstX = Mathf.Abs(nodeA.gridX - nodeB.gridX);
+        int dstZ = Mathf.Abs(nodeA.gridZ - nodeB.gridZ);
 
-
-
-        int distanceX = Mathf.Abs(nodeA.gridX - nodeB.gridX);
-        int distanceZ = Mathf.Abs(nodeA.gridZ - nodeB.gridZ);
-
-        if (distanceX > distanceZ)
-        
-            return 14 * distanceZ + 10 * (distanceX - distanceZ);
-        return 14 * distanceX + 10 * (distanceZ - distanceX);
-        }
+        if (dstX > dstZ)
+            return 14 * dstZ + 10 * (dstX - dstZ);
+        return 14 * dstX + 10 * (dstZ - dstX);
+    }
 }
