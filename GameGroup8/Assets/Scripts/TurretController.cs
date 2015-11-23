dd9e0f6cc52c58a124f12af11d595093c6674ccc
SirHenk <heddevanderweg@hotmail.com>
2015-11-23 17:20:31 +0100

    Turret implementation (begin)

---
+++ GameGroup8/Assets/Scripts/TurretController.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TurretController : MonoBehaviour {
+
+    private GameObject Vijand;
+    private Vector3 VijandPositie;
+
+    void Start()
+    {
+
+    }
+    
+    void Update()
+    {
+        if (Vijand)
+        {
+            VijandPositie = Vijand.transform.position;
+            transform.LookAt(VijandPositie);
+            Debug.Log(VijandPositie);
+        }
+        
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Enemy")
+        {
+            Vijand = other.gameObject;
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "Enemy")
+        {
+            Vector3 LastPostition = other.transform.position;
+            VijandPositie = LastPostition;
+            Vijand = null;
+        }
+         
+    }
+}
