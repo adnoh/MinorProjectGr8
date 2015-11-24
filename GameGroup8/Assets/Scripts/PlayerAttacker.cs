49fab116228cc01858b1688a6e000c3121025eaf
wroel <wytze.roel@gmail.com>
2015-11-24 12:14:19 +0100

    Fix damage done by merge

--- GameGroup8/Assets/Scripts/PlayerAttacker.cs
+++ GameGroup8/Assets/Scripts/PlayerAttacker.cs
@@ -14,6 +14,11 @@ public class PlayerAttacker : MonoBehaviour {
 
 	public GameObject bullet;
 	public float bulletSpeed = 100f;
+
+	public float attackRate = 0.5f;
+	private float nextAttack = 0.0f;
+
+	public static EnemyController lastAttackedEnemy;
 		
 	void Start () {
 		showEnemyDescription = false;
@@ -23,39 +28,19 @@ public class PlayerAttacker : MonoBehaviour {
 
 	void Update () {
 		enemyDescription.SetActive (showEnemyDescription);
-        bool Base = Controls.getPause();
+        bool Base = PlayerController.getPause();
+		if (lastAttackedEnemy != null) {
+			setEnemyDescription (lastAttackedEnemy);
+		} else {
+			showEnemyDescription = false;
+		}
 
         if (!Base)
         {
-            if (Input.GetMouseButtonDown(0))
-            {
-
-                GameObject shot = GameObject.Instantiate(bullet, transform.position + (transform.forward), transform.rotation) as GameObject;
+			if (Input.GetMouseButtonDown(0) && Time.time > nextAttack ){
+				nextAttack = Time.time + attackRate;
+				GameObject shot = GameObject.Instantiate(bullet, transform.position + (transform.forward), transform.rotation) as GameObject;
                 shot.GetComponent<Rigidbody>().AddForce(transform.forward * bulletSpeed);
-            }
-
-
-            if (Input.GetMouseButtonDown(2) && false)
-            {
-
-                RaycastHit hit;
-                Ray bulletray = new Ray(transform.position, Vector3.forward);
-                if (Physics.Raycast(bulletray, out hit))
-                {
-                    if (hit.collider.tag == "Enemy")
-                    {
-                        EnemyController enemyController = hit.collider.gameObject.GetComponent<EnemyController>();
-                        enemyController.setHealth(enemyController.getHealth());
-
-
-                        if (enemyController.getHealth() <= 0)
-                        {
-                            EnemySpawner.enemiesDefeaten++;
-                            Destroy(hit.collider.gameObject);
-                            showEnemyDescription = false;
-                        }
-                    }
-                }
             }
         }
 		
