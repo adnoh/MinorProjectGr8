7595f463adfbe65fee16b34b49a2fb176788d5a7
SirHenk <heddevanderweg@hotmail.com>
2015-12-09 12:17:08 +0100

    (semi-)Cool world generator

--- GameGroup8/Assets/Scripts/WorldGen/TDMapII.cs
+++ GameGroup8/Assets/Scripts/WorldGen/TDMapII.cs
@@ -28,22 +28,26 @@ public class TDMapII {
         villages = new int[nrVillages][];
 
         MakeLand();
-        PlaceBase();
+        int[] BasePos = PlaceBase();
         MakeWater();
         for (int i = 0; i < nrForr; i++)
         {
             MakeForrests(i);
         }
-        for (int i = 0; i < nrVillages; i++)
-        {
-            PlaceVillages(i);
-        }
+
+        int[] VPos = PlaceVillages(0);
         
+        makePrimaryRoads(BasePos[0], BasePos[1], VPos[0], VPos[1]);
     }
 
     public int getTile(int x, int y)
     {
         return tiles[y][x];
+    }
+
+    public void setTile(int x, int y, int value)
+    {
+        tiles[y][x] = value;
     }
 
     public int[][] getForrests()
@@ -62,12 +66,29 @@ public class TDMapII {
         {
             for (int x = 0; x < size_x; x++)
             {
-                tiles[y][x] = 1;
+                float texture = Random.Range(0f, 10f);
+                if (texture <= 5)
+                {
+                    tiles[y][x] = 1;
+                }
+                else if (texture > 5 && texture <= 7.5)
+                {
+                    tiles[y][x] = 2;
+                }
+                else if (texture >= 7.5)
+                {
+                    tiles[y][x] = 3;
+                }
             }
         }
+
+        tiles[3][6] = 5;
+        Debug.Log("tiles[3][6] = white;");
+        tiles[6][3] = 4;
+        Debug.Log("tiles[6][3] = black");
     }
 
-    void PlaceBase()
+    int[] PlaceBase()
     {
         int x_pos = (int)Mathf.Floor(size_x / 2) + Random.Range(-10, 10);
         int y_pos = (int)Mathf.Floor(size_y / 2) + Random.Range(-10, 10);
@@ -79,6 +100,8 @@ public class TDMapII {
                 tiles[y + y_pos][x + x_pos] = 4;
             }
         }
+
+        return new int[2] { x_pos, y_pos };
     }
 
     void MakeWater()
@@ -91,6 +114,9 @@ public class TDMapII {
                 bottom_offset = 2;
             if (bottom_offset > 10)
                 bottom_offset = 9;
+
+            tiles[y][size_x - bottom_offset - 1] = 11;
+            tiles[y][size_x - bottom_offset - 2] = 11;
             for (int x = size_x - bottom_offset; x < size_x; x++)
             {
                 tiles[y][x] = 0;
@@ -105,7 +131,10 @@ public class TDMapII {
                 top_offset = 2;
             if (top_offset > 10)
                 top_offset = 9;
-            for (int x = 0; x < top_offset - 1; x++)
+
+            tiles[y][top_offset + 1] = 11;
+            tiles[y][top_offset] = 11;
+            for (int x = 0; x < top_offset; x++)
             {
                 tiles[y][x] = 0;
             }
@@ -119,7 +148,12 @@ public class TDMapII {
                 right_offset = 2;
             if (right_offset > 10)
                 right_offset = 9;
-            for (int y = 0; y < right_offset - 1; y++)
+
+            if (tiles[right_offset + 1][x] != 0)
+                tiles[right_offset + 1][x] = 11;
+            if (tiles[right_offset][x] != 0)
+                tiles[right_offset][x] = 11;
+            for (int y = 0; y < right_offset; y++)
             {
                 tiles[y][x] = 0;
             }
@@ -133,6 +167,11 @@ public class TDMapII {
                 left_offset = 2;
             if (left_offset > 10)
                 left_offset = 9;
+
+            if (tiles[size_y - left_offset - 1][x] != 0)
+                tiles[size_y - left_offset - 1][x] = 11;
+            if (tiles[size_y - left_offset - 2][x] != 0)
+                tiles[size_y - left_offset - 2][x] = 11;
             for (int y = size_y - left_offset; y < size_y; y++)
             {
                 tiles[y][x] = 0;
@@ -160,7 +199,7 @@ public class TDMapII {
         forrests[i] = new int[4] { x_pos,y_pos,min,max };
     }
 
-    void PlaceVillages(int i)
+    int[] PlaceVillages(int i)
     {
         int x_pos = Random.Range(50, 250);
         int y_pos = Random.Range(50, 250);
@@ -168,14 +207,40 @@ public class TDMapII {
         int max = Random.Range(12, 25);
         int min = -1 * max;
 
-        for (int y = min; y < max; y++)
+        int maxMarket = (int)Mathf.Floor(max/2);
+
+        for (int y = 0; y < maxMarket; y++)
         {
-            for (int x = min; x < max; x++)
+            for (int x = 0; x < maxMarket; x++)
             {
                 tiles[y + y_pos][x + x_pos] = 10;
             }
         }
 
         villages[i] = new int[4] { x_pos, y_pos, min, max };
+
+        return new int[2] { x_pos, y_pos };
+    }
+
+    void makePrimaryRoads(int xPosB, int yPosB, int xPosV, int yPosV)
+    {
+        int y1 = yPosB + 1;
+        int x1 = xPosB + 5;
+
+        int x2 = xPosV;
+        int y2 = yPosV;
+
+        while (x1 != x2)
+        {
+            tiles[y1][x1] = 7;
+            x1 += x1 < x2 ? 1 : -1;
+        }
+
+        while (y1 != y2)
+        {
+            tiles[y1][x1] = 7;
+            y1 += y1 < y2 ? 1 : -1;
+        }
+
     }
 }
