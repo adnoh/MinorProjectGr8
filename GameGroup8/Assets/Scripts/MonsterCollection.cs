d55eb34caabfcead6b5e8f4b15548bde176686cb
wroel <wytze.roel@gmail.com>
2015-12-15 09:41:31 +0100

    Add serializer for outside

--- GameGroup8/Assets/Scripts/MonsterCollection.cs
+++ GameGroup8/Assets/Scripts/MonsterCollection.cs
@@ -22,6 +22,39 @@ public class MonsterList
 
 }
 
+public class TurretList{
+	
+	[XmlArray("list"),XmlArrayItem("list")]
+	public Turret[]  list = new Turret[10];
+}
+
+public class Outsidesave{
+	
+	public int wave;
+	public float timeTillNextWave;
+	public int enemiesDefeaten;
+	
+	public string tagOfMat1;
+	public string tagOfMat2;
+	public string tagOfMat3;
+	public string tagOfMat4;
+	
+	public int unitCount;
+
+	public Outsidesave(){
+		wave = Camera.main.GetComponent<EnemySpawner> ().wave;
+		timeTillNextWave = Camera.main.GetComponent<EnemySpawner> ().timeTillNextWave;
+		enemiesDefeaten = EnemySpawner.enemiesDefeaten;
+		tagOfMat1 = GameObject.Find ("PlacementPlane").tag;
+		tagOfMat2 = GameObject.Find ("PlacementPlane(1)").tag;
+		tagOfMat3 = GameObject.Find ("PlacementPlane(2)").tag;
+		tagOfMat4 = GameObject.Find ("PlacementPlane(3)").tag;
+		unitCount = PlayerController.getCount();
+	}
+	
+	
+}
+
 public class Player
 {
 	public float posx;
@@ -53,15 +86,11 @@ public class Player
 public class MonsterCollection : MonoBehaviour
 {
 	public static MonsterList monsterlist = new MonsterList();
+	public static TurretList turretList = new TurretList();
 
 	//[XmlArray("monstersList"),XmlArrayItem("monstersList")]
 	//public Monster[] monstersList = new Monster[2];
-
-	void Start(){
-
-
-
-	}
+	
 	 void Update(){
 
 		if (Input.GetKeyDown (KeyCode.UpArrow)) {
@@ -98,9 +127,20 @@ public class MonsterCollection : MonoBehaviour
 		}
 	}
     */
+
+	public void outsideSave(string path){
+		List<BuildingController> Buildings = BaseController.turrets;
+		for (int i = 0; i < Buildings.Count; i++){
+			turretList.list[i] = new Turret(Buildings[i].GetComponent<BuildingController>());
+		}
+		
+		var serializer = new XmlSerializer(typeof(Turret));
+		using(var stream = new FileStream(path, FileMode.Create)){
+			serializer.Serialize(stream, turretList);
+		}
+	}
 	
-	public static MonsterList MonsterLoad(string path)
-	{
+	public static MonsterList MonsterLoad(string path){
 		var serializer = new XmlSerializer(typeof(MonsterList));
 		using(var stream = new FileStream(path, FileMode.Open))
 		{
