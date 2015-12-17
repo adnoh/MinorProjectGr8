2e33b6c3b8a0ec55a27de8c4d5b8c5bff0eb775b
Mr-Pingu <karstenvanherwaarden@gmail.com>
2015-12-17 11:14:38 +0100

    newgame and loadgame functions

--- GameGroup8/Assets/Scripts/NewGame.cs
+++ GameGroup8/Assets/Scripts/NewGame.cs
@@ -3,8 +3,30 @@ using System.Collections;
 
 public class NewGame : MonoBehaviour {
 
+    // NewGame or not? setter
     public void setNGame(bool newgame_)
     {
         GameStateController.setNewgame(newgame_);
     }
+
+    // NewGamge
+    void New_Game()
+    {
+        setNGame(true);
+        loadoutside();
+
+    }
+
+    // LoadGame
+    void LoadGame()
+    {
+        setNGame(false);
+        loadoutside();
+    }
+
+    public void loadoutside()
+    {
+        Application.LoadLevel(1);
+    }
+ 
 }
