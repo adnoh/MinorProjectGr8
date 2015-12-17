23e1d6c69f255f78a61243b87bc51a750a1ba14d
Cleanlycashew <renehiemstra@hotmail.com>
2015-12-17 10:13:55 +0100

    main menu

---
+++ GameGroup8/Assets/Audioslider.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class Audioslider : MonoBehaviour {
+
+
+	// Update is called once per frame
+	void Update () {
+		/*
+		var audio = Camera.main.GetComponent <AudioSource>();
+
+	
+		var slider = gameObject.GetComponent<Slider> ();
+
+		Debug.Log (slider.value);
+		float temp = slider.value;
+
+		audio.volume=temp;
+*/
+	}
+}
---
+++ GameGroup8/Assets/Graphicsslider.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+
+public class Graphicsslider : MonoBehaviour {
+
+
+	// Update is called once per frame
+	void Update () {
+
+		 var slider = gameObject.GetComponent <Slider>();
+		var text = GetComponentInChildren<Text> ();
+
+	
+
+		int caseSwitch = Mathf.CeilToInt(slider.value);
+		switch (caseSwitch)
+		{
+		case 0:
+			QualitySettings.currentLevel = QualityLevel.Fastest;
+			text.text=  "Fastest";
+			break;
+		case 1:
+			QualitySettings.currentLevel = QualityLevel.Fast;
+			text.text=  "Fast";
+			break;
+		case 2:
+			QualitySettings.currentLevel = QualityLevel.Simple;
+			text.text=  "Simple";
+			break;
+		case 3:
+			QualitySettings.currentLevel = QualityLevel.Good;
+			text.text=  "Good";
+			break;
+		case 4:
+			QualitySettings.currentLevel = QualityLevel.Beautiful;
+			text.text=  "Beautiful";
+			break;
+		case 5:
+			QualitySettings.currentLevel = QualityLevel.Fantastic;
+			text.text=  "Fantastic";
+			break;
+		default:
+			QualitySettings.currentLevel = QualityLevel.Fast;
+			text.text=  "you broke the system......";
+			break;
+		}
+	}
+}
