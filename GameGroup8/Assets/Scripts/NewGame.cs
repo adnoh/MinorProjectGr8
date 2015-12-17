ec8fe32bd6ad7fe71b22ee4715f75078fc52af87
Mr-Pingu <karstenvanherwaarden@gmail.com>
2015-12-17 11:11:12 +0100

    newgame and loadgame functions

---
+++ GameGroup8/Assets/Scripts/NewGame.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NewGame : MonoBehaviour {
+
+    public void setNGame(bool newgame_)
+    {
+        GameStateController.setNewgame(newgame_);
+    }
+}
