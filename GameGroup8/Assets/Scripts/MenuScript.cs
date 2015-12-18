fe41a307d3ac4069e5ea2f2e509feb88e3d38610
wroel <wytze.roel@gmail.com>
2015-12-18 16:55:08 +0100

    Fix main menu and pausemenu

--- GameGroup8/Assets/Scripts/MenuScript.cs
+++ GameGroup8/Assets/Scripts/MenuScript.cs
@@ -5,23 +5,5 @@ using System.Collections;
 
 public class MenuScript : MonoBehaviour {
 
-	public Canvas AreYouSureMenu;
-
-	public void newGame(){
-
-	}
-
-	public void ExitPress(){
-		AreYouSureMenu.enabled = true;
-	}
-
-	public void ExitGame(){
-		Debug.Log (true);
-		Application.Quit ();
-	}
-
-	public void NoPress(){
-		AreYouSureMenu.enabled = false;
-	}
 
 }
