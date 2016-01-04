5668554dc271d949665d7c96685479d6e3a7c6e9
ko102 <marco.a1881@gmail.com>
2016-01-04 16:55:40 +0100

    tutorial scene

---
+++ GameGroup8/Assets/TutorialScript.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class TutorialScript : MonoBehaviour {
+
+    public Text Tutorialtext;
+
+
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
