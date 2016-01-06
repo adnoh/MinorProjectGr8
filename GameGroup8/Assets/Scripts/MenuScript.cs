a97c2b8e19fbc434568cf17ac4ae5c27640fb489
ko102 <marco.a1881@gmail.com>
2016-01-06 12:35:59 +0100

    eerste animaties in main menu

--- GameGroup8/Assets/Scripts/MenuScript.cs
+++ GameGroup8/Assets/Scripts/MenuScript.cs
@@ -2,8 +2,21 @@
 using UnityEngine.UI;
 using UnityEngine.Audio;
 using System.Collections;
+using UnityEngine.SceneManagement;
 
 public class MenuScript : MonoBehaviour {
 
+    public Animator backdrop;
+    public Animator buttons;
 
+
+
+    public void animationplay()
+    {
+        backdrop = GetComponent<Animator>();
+        buttons = GetComponent<Animator>();
+        backdrop.SetBool("animatorconstant", true);
+        buttons.SetBool("start game", true);
+
+    }
 }
