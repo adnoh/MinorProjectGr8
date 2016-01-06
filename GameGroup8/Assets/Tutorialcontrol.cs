5ab307b209637bf1650cd310a0e2ced26f2b4595
Cleanlycashew <renehiemstra@hotmail.com>
2016-01-06 14:32:31 +0100

    tutorials!

---
+++ GameGroup8/Assets/Tutorialcontrol.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Tutorialcontrol : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        var audio = Camera.main.GetComponent<AudioSource>();
+        audio.volume = PlayerPrefs.GetFloat("sound option");
+
+        QualitySettings.SetQualityLevel(PlayerPrefs.GetInt ("grahpics option"), true);
+    }
+}
