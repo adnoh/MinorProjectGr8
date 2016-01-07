7823287b5cb23b705e5f43f2411f78f8859e482c
wroel <wytze.roel@gmail.com>
2016-01-07 16:51:52 +0100

    Improve basic AI

--- GameGroup8/Assets/Scripts/Enemy/EnemyController.cs
+++ GameGroup8/Assets/Scripts/Enemy/EnemyController.cs
@@ -18,6 +18,7 @@ public class EnemyController : MonoBehaviour {
 	public int maxHealth;
 	public int attackPower;
 	public float walkingSpeed;
+    public float updatedSpeed;
 	public Type type;
 
     public Vector3 position;
@@ -73,6 +74,7 @@ public class EnemyController : MonoBehaviour {
         health = enemy.getMaxHealth();
         attackPower = enemy.getAttackPower();
         walkingSpeed = enemy.getWalkingSpeed();
+        updatedSpeed = enemy.getWalkingSpeed();
         type = enemy.getType();
 
         isWithinRange = false;
@@ -87,6 +89,7 @@ public class EnemyController : MonoBehaviour {
     }
 
 	void Update () {
+        updatedSpeed = walkingSpeed * (1f + (0.6f * (Analytics.getHitCount()[0] / (Analytics.getHitCount()[1] + Analytics.getHitCount()[0]) - 0.5f)));
         healthBarClone.transform.position = Camera.main.WorldToScreenPoint(gameObject.transform.position) + new Vector3(0, 20, 0);
         descriptionClone.transform.position = Camera.main.WorldToScreenPoint(gameObject.transform.position) + new Vector3(0, 25, 0);
         healthBarClone.value = health;
@@ -101,7 +104,7 @@ public class EnemyController : MonoBehaviour {
             this.gameObject.transform.LookAt(GameObject.Find("player").transform.position);
             if (!isWithinRange)
             {
-                transform.position = Vector3.MoveTowards(transform.position, PlayerController.getPosition(), walkingSpeed * Time.deltaTime);
+                transform.position = Vector3.MoveTowards(transform.position, PlayerController.getPosition(), updatedSpeed * Time.deltaTime);
             }
         }
 		if (Time.time > nextAttack) {
@@ -307,7 +310,9 @@ public class EnemyController : MonoBehaviour {
 		anim.SetBool ("dying", true);
         yield return new WaitForSeconds(1);
         PSpawner spawner = Camera.main.GetComponent<PSpawner>();
-        spawner.placeUnit(this.gameObject.transform.position);
+        if (Random.Range(0f, 1f) > Analytics.get_timeCTBase() / Analytics.get_timePlayed()) {
+            spawner.placeUnit(this.gameObject.transform.position);
+        }
         healthBarClone.transform.position = new Vector3(-1000, -1000, 0);
         descriptionClone.transform.position = new Vector3(-1000, -1000, 0);
         destroyed = true;
