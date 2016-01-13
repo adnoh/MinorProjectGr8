2b73031869617d7fdb6a69a78b63b398afde12af
Cleanlycashew <renehiemstra@hotmail.com>
2016-01-13 11:42:22 +0100

    mockup

--- GameGroup8/Assets/Audioslider.cs
+++ GameGroup8/Assets/Audioslider.cs
@@ -3,6 +3,9 @@ using System.Collections;
 using UnityEngine.UI;
 
 public class Audioslider : MonoBehaviour {
+
+  
+    public AudioSource backaudio;
 	void Start() {
 		var slider = gameObject.GetComponent<Slider> ();
 		var temp = PlayerPrefs.GetFloat ("sound option");
@@ -14,17 +17,19 @@ public class Audioslider : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 
-		var audio = Camera.main.GetComponent <AudioSource>();
+        
+        var back = backaudio.GetComponent<AudioSource>();
 
-	
-		var slider = gameObject.GetComponent<Slider> ();
+
+        var slider = gameObject.GetComponent<Slider> ();
 
 
 		float temp = slider.value;
+      
+       
+        back.volume = temp;
 
-		audio.volume = temp;
-
-		PlayerPrefs.SetFloat("sound option", temp);
+        PlayerPrefs.SetFloat("sound option", temp);
 		PlayerPrefs.Save();
 		// Debug.Log (temp);
 
---
+++ GameGroup8/Assets/Scripts/Menu's/sfxslider.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class sfxslider : MonoBehaviour
+{
+
+    public AudioSource zoomaudio;
+    
+    void Start()
+    {
+        var slider = gameObject.GetComponent<Slider>();
+        var temp = PlayerPrefs.GetFloat("sfx option");
+        slider.value = temp;
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        var buttonaudio = Camera.main.GetComponent<AudioSource>();
+        var zoom = zoomaudio.GetComponent<AudioSource>();
+      
+
+
+        var slider = gameObject.GetComponent<Slider>();
+
+
+        float temp = slider.value;
+        zoom.volume = temp;
+        buttonaudio.volume = temp;
+       
+
+        PlayerPrefs.SetFloat("sfx option", temp);
+        PlayerPrefs.Save();
+        // Debug.Log (temp);
+
+    }
+}
