ba27c96d6b59318911fab09f5e4ec07b05dcb2fe
SirHenk <heddevanderweg@hotmail.com>
2016-01-13 12:27:39 +0100

    Begin sound outside

---
+++ GameGroup8/Assets/Scripts/Player/SoundsPlayer.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public class SoundsPlayer : MonoBehaviour {
+
+    public AudioClip walk;
+    public AudioClip[] stab = new AudioClip[2];
+    public AudioClip dead;
+
+	void Start () {
+	
+	}
+	
+	void Update () {
+	
+	}
+}
---
+++ GameGroup8/Assets/Scripts/Player/SoundsWeapons.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+public class SoundsWeapons : MonoBehaviour {
+
+    public AudioClip[] soundsBaseBat = new AudioClip[3];
+    public AudioClip[] soundsPistol = new AudioClip[1];
+    public AudioClip[] soundsRam = new AudioClip[2];
+    public AudioClip[] soundsStinger = new AudioClip[1];
+    public AudioClip[] soundsSwordFish = new AudioClip[3];
+    public AudioClip[] soundsEel = new AudioClip[2];
+    public AudioClip[] soundsWunderWaffe = new AudioClip[1];
+
+	// Use this for initialization
+	void Start () {
+        
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
