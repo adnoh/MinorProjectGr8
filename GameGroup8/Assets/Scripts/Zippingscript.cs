e9262de87ad56509663825af0f6f28d9afe6209d
Mr-Pingu <karstenvanherwaarden@gmail.com>
2016-01-18 15:31:58 +0100

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	GameGroup8/Assets/Scripts/Zippingscript.cs

--- GameGroup8/Assets/Scripts/Zippingscript.cs
+++ GameGroup8/Assets/Scripts/Zippingscript.cs
@@ -19,11 +19,11 @@ using Ionic.Zip;
 
     public static void Decompress()
     {
-        using (ZipFile zip = ZipFile.Read("Assets/saves/Package.zip"))
+        using (ZipFile zip = ZipFile.Read(Application.dataPath+ "saves/Package.zip"))
             {
               foreach (ZipEntry e in zip)
                   {
-                e.Extract("Assets/saves", ExtractExistingFileAction.OverwriteSilently);
+                e.Extract(Application.dataPath + "saves/Package.zip", ExtractExistingFileAction.OverwriteSilently);
             }
             }
 
@@ -34,15 +34,15 @@ using Ionic.Zip;
        
         using (ZipFile zip = new ZipFile())
             {
-              zip.AddFile("Assets/saves/base.xml","");
-              zip.AddFile("Assets/saves/monsters.xml", "");
-             zip.AddFile("Assets/saves/moon.xml", "");
-            zip.AddFile("Assets/saves/outside.xml", "");
-            zip.AddFile("Assets/saves/Player.xml", "");
-            zip.AddFile("Assets/saves/sun.xml", "");
-            zip.AddFile("Assets/saves/turrets.xml", "");
-            zip.AddFile("Assets/saves/world.xml", "");
-            zip.Save("Assets/saves/Package.zip", "");
-           }
+              zip.AddFile(Application.dataPath + "saves/base.xml","");
+              zip.AddFile(Application.dataPath + "saves/monsters.xml", "");
+             zip.AddFile(Application.dataPath + "saves/moon.xml", "");
+            zip.AddFile(Application.dataPath + "saves/outside.xml", "");
+            zip.AddFile(Application.dataPath + "saves/Player.xml", "");
+            zip.AddFile(Application.dataPath + "saves/sun.xml", "");
+            zip.AddFile(Application.dataPath + "saves/turrets.xml", "");
+            zip.AddFile(Application.dataPath + "saves/world.xml", "");
+            zip.Save(Application.dataPath + "saves/Package.zip");
+        }
     }
     }
