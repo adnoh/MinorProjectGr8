0a9f78f76628f8ac6e9981559b430d96333097a8
wroel <wytze.roel@gmail.com>
2016-01-22 16:05:43 +0100

    Add comments to camerController

--- GameGroup8/Assets/Scripts/Player/CameraController.cs
+++ GameGroup8/Assets/Scripts/Player/CameraController.cs
@@ -19,56 +19,53 @@ public class CameraController : MonoBehaviour {
     private int movingSpeed = 15;
 
 	public static bool shaking;
-	
+
+	/// <summary>
+	/// the offset between the player and the camera.
+	/// </summary>
 	void Start (){
 		offset = transform.position - player.transform.position;
         moving = false;
         saveLocation = false;
         whatMove = 0;
     }
-	
+
+	/// <summary>
+	/// Translates the camera when the player moves.
+	/// </summary>
 	void LateUpdate (){
 		BaseEnter = BaseController.pause;
-
-        switch (whatMove)
-        {
-            case 1:
-                moveCameraToBase();
-                break;
-            case 2:
-                moveCameraToPlayer();
-                break;
-            default:
-                if (!BaseEnter && !shaking)
-                {
-                    transform.position = player.transform.position + offset;
-                }
-                else if (BaseEnter)
-                {
-                    transform.position = GameObject.FindGameObjectWithTag("BASE").transform.position + new Vector3(0, 20, -6);
-                }
-                break;
-        }
-
+		switch (whatMove) {
+		case 1:
+			moveCameraToBase ();
+			break;
+		case 2:
+			moveCameraToPlayer ();
+			break;
+		default:
+			if (!BaseEnter && !shaking) {
+				transform.position = player.transform.position + offset;
+			} else if (BaseEnter) {
+				transform.position = GameObject.FindGameObjectWithTag ("BASE").transform.position + new Vector3 (0, 20, -6);
+			}
+			break;
+		}
 	}
 
     /// <summary>
     /// Move camera from the player position to the base;
     /// </summary>
-    void moveCameraToBase()
-    {
-        if(saveLocation == false)
-        {
+    void moveCameraToBase(){
+        if(saveLocation == false){
             saveLocation = true;
             tempLocation = gameObject.transform.position;
             transformCamera = ((GameObject.FindGameObjectWithTag("BASE").transform.position + new Vector3(0, 20, -6)) - tempLocation)/movingSpeed;
         }
 
-        if (moving == true)
-            gameObject.transform.position += transformCamera;
-        
-        if (Vector3.Distance(gameObject.transform.position,GameObject.FindGameObjectWithTag("BASE").transform.position + new Vector3(0, 20, -6)) < 0.1)
-        {
+		if (moving == true) {
+			gameObject.transform.position += transformCamera;
+		}
+        if (Vector3.Distance(gameObject.transform.position,GameObject.FindGameObjectWithTag("BASE").transform.position + new Vector3(0, 20, -6)) < 0.1){
             moving = false;
             saveLocation = false;
             whatMove = 0;
@@ -78,19 +75,16 @@ public class CameraController : MonoBehaviour {
     /// <summary>
     /// move the camera from the base to the player
     /// </summary>
-    void moveCameraToPlayer()
-    {
-        if (saveLocation == false)
-        {
+    void moveCameraToPlayer(){
+        if (saveLocation == false){
             saveLocation = true;
             transformCamera = (tempLocation - (GameObject.FindGameObjectWithTag("BASE").transform.position + new Vector3(0, 20, -6)))/movingSpeed;
         }
 
-        if (moving == true)
-            gameObject.transform.position += transformCamera;
-
-        if (Vector3.Distance(gameObject.transform.position, tempLocation) < 0.1)
-        {
+		if (moving == true) {
+			gameObject.transform.position += transformCamera;
+		}
+        if (Vector3.Distance(gameObject.transform.position, tempLocation) < 0.1){
             moving = false;
             saveLocation = false;
             whatMove = 0;
@@ -101,10 +95,8 @@ public class CameraController : MonoBehaviour {
     /// say how the camera should move (1 to base, 2 from base)
     /// </summary>
     /// <param name="i"></param>
-    public void MoveCamera(int i)
-    {
-        if (moving == false)
-        {
+    public void MoveCamera(int i){
+        if (moving == false){
             moving = true;
             whatMove = i;
         }
